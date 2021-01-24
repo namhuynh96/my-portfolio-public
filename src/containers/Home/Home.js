1c44a1c10ea4e2ee0a645aadaf5475a9d3c2eb5e
Nam Huynh <huynhchinam96@gmail.com>
2021-01-24 23:41:43 +0200

    Add animation to home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -7,11 +7,13 @@ const Home = () => {
   return (
     <Section>
       <div className={classes.Home}>
-        <h2>Hi, I am Nam 👋</h2>
+        <h2 className={classes.Greeting}>Hi, I am Nam 👋</h2>
         <h1 className={classes.Title}>A full stack developer.</h1>
-        <h1>Love the entrepreneur spirit.</h1>
-        <h1>And create amazing things on the Internet.</h1>
-        <a href="#contact">
+        <div className={classes.Intro}>
+          <h1>Love the entrepreneur spirit.</h1>
+          <h1>And create amazing things on the Internet.</h1>
+        </div>
+        <a href="#contact" className={classes.ConnectBtn}>
           <Button>Connect with me</Button>
         </a>
       </div>
